a26e64378ceb29b09f86d1cdb44ec424adde2df0
sebsverastegui <sebas.paps.12@gmail.com>
2024-11-16 03:47:20 -0600

    80% aestethic

---
+++ PRank/Views/Categories/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  PRank
+//
+//  Created by seb's on 11/16/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItem()
+}
---
+++ PRank/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  PRank
+//
+//  Created by seb's on 11/16/24.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
