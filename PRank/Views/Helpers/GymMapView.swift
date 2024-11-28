bbcf9220af1c79f1c04b4c2d5c06fb195a279aea
sebsverastegui <sebas.paps.12@gmail.com>
2024-11-28 02:46:40 -0600

    APERTURA DE MAP VERSION 1.0

---
+++ PRank/Views/Helpers/GymMapView.swift
@@ -0,0 +1,18 @@
+//
+//  GymMapView.swift
+//  PRank
+//
+//  Created by seb's on 11/27/24.
+//
+
+import SwiftUI
+
+struct GymMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GymMapView()
+}
